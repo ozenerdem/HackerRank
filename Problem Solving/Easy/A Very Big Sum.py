a1fde58592027e4a12d5cd476d88fd4d1dd2586e
Erdem OZEN <ozenerdem98@gmail.com>
2021-01-22 22:46:57 +0300

    Training on HackerRank

--- Problem Solving/Easy/A Very Big Sum.py
+++ Problem Solving/Easy/A Very Big Sum.py
@@ -1,12 +1,8 @@
 """
 In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
-
 Function Description
-
 Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
-
 aVeryBigSum has the following parameter(s):
-
 int ar[n]: an array of integers .
 Sample Input
 
@@ -16,7 +12,6 @@ Output
 
 5000000015
 """
-
 if __name__ == '__main__':
     n = int(input())
     ar = map(int, input().split())
